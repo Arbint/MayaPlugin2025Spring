55a7bb36b69eb306da443f449796897ebc1d6584
jili1@uiwtx.edu <jili1@uiwtx.edu>
2025-04-29 14:48:54 -0500

    added animation importing in unreal utilities

--- src/UnrealUtils.py
+++ src/UnrealUtils.py
@@ -29,8 +29,34 @@ def ImportSkeletalMesh(meshPath):
     unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks([importTask])
     return importTask.get_objects()[-1]
 
+def ImportAnimation(mesh, animPath):
+    importTask = CreateBaseImportTask(animPath)
+    meshDir = os.path.dirname(mesh.get_path_name())
+    importTask.destination_path = meshDir + "/animations"
+
+    importOptions = unreal.FbxImportUI()
+    importOptions.import_mesh = False
+    importOptions.import_as_skeletal = True
+    importOptions.import_animations = True
+    importOptions.skeleton = mesh.skeleton
+
+    importOptions.set_editor_property('automated_import_should_detect_type', False)
+    importOptions.set_editor_property('original_import_type', unreal.FBXImportType.FBXIT_SKELETAL_MESH)
+    importOptions.set_editor_property('mesh_type_to_import', unreal.FBXImportType.FBXIT_ANIMATION)
+
+    importTask.options = importOptions
+
+    unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks([importTask])
+
+
 def ImportMeshAndAnimations(meshPath, animDir):
     mesh = ImportSkeletalMesh(meshPath) 
     print(mesh)
+
+    for file in os.listdir(animDir):
+        animPath = os.path.join(animDir, file)
+        ImportAnimation(mesh, animPath)
+
+
 
 ImportMeshAndAnimations("D:/MayaToUETemp/Alex.fbx", "D:/MayaToUETemp/animations/")
